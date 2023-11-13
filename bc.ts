ddc2daaf59d170daca04513a68f8773791a32302
see2et <seezet1024@gmail.com>
2023-11-13 16:01:06 +0900

    better cli

--- bc.ts
+++ bc.ts
@@ -1,10 +1,21 @@
+import { readLines } from "https://deno.land/std/io/mod.ts";
+import { writeAllSync } from "https://deno.land/std/streams/mod.ts";
 import { sendWebhook } from "./lib/sender.ts";
 
-const id = Deno.args[0];
-const content = Deno.args.reduce((a, b, i) => {
-  if (i === 0) return "";
-  if (i === 1) return b;
-  return a + b;
-});
+const prompt = new TextEncoder().encode("> ");
+writeAllSync(Deno.stdout, prompt);
 
-sendWebhook(id, content);
+for await (let line of readLines(Deno.stdin)) {
+  const args = line.split(" ");
+  const id = args[0];
+  const content = args.reduce((a, b, i) => {
+    if (i === 0) return "";
+    if (i === 1) return b;
+    return a + b;
+  });
+
+  sendWebhook(id, content);
+
+  const prompt = new TextEncoder().encode("> ");
+  writeAllSync(Deno.stdout, prompt);
+}
